93b72a33ddbf73d606d5da3458d297936e65f3e8
DeStRaaM <20destraam03@gmail.com>
2024-11-24 23:58:35 +0100

    fix

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -87,7 +87,7 @@ export const getCurrentUser = async () => {
 
 		return parseStringify(user.documents[0]);
 	} catch (error) {
-		console.log(error);
+		handleError(error, 'Failed to get current user');
 	}
 };
 
